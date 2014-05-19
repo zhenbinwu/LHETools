e076358ffa2fb3c0b1d00ed18027a6d1b653fda9
benwu <benwu@fnal.gov>
2014-05-19 08:38:55 -0500

    Support reading gzip file directly

--- Reader/LHEfile.py
+++ Reader/LHEfile.py
@@ -6,15 +6,20 @@ class LHEfile():
         self.eventList = []
         self.fileINname = fileINname
         self.MaxEv = -99
+        self.fileIN = 0
 
     def setMax(self, maxVal):
         self.Max = maxVal
         
     def readEvents(self):
-        fileIN = open(self.fileINname)
+        if ".gz" in self.fileINname:
+            import gzip
+            self.fileIN = gzip.open(self.fileINname, 'r')
+        else:
+            self.fileIN = open(self.fileINname)
         newEVENT = False
         oneEvent = []
-        for line in fileIN:
+        for line in self.fileIN:
             if newEVENT: oneEvent.append(line)
             if line.find("</event>") != -1:
                 # the event block ends
@@ -26,5 +31,5 @@ class LHEfile():
                 # the event block starts
                 newEVENT = True
                 oneEvent.append(line)
-        fileIN.close()
+        self.fileIN.close()
         return self.eventList
